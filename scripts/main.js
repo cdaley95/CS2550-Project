bf7407c6687f7e8a8ee31c43a9a8f9ef0885e32f
cdaley95 <10672938@uvu.edu>
2024-07-17 13:43:32 -0600

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,43 @@ function showSection(sectionId) {
         }
     });
 }
+
+function changeTheme() {
+    const themeOverride = document.getElementById('themeOverride');
+    const button = document.getElementById('themebtn');
+    const bgVid = document.getElementById('bgVid');
+    const bgVideo = document.getElementById('bgVideo');
+
+    if (themeOverride.disabled) {
+        bgVid.src = "videos/bgviddark.mp4";
+        bgVideo.load();
+        themeOverride.disabled = false;
+        button.textContent = 'Light Mode';
+    } else {
+        bgVid.setAttribute("src", "videos/bgvid.mp4");
+        bgVideo.load();
+        themeOverride.disabled = true;
+        button.textContent = 'Dark Mode';
+    }
+}
+
+function moveThemeButton() {
+    const themeBtn = document.getElementById('themebtn');
+    const footerContent = document.querySelector('.footer-content');
+    const paddedCont = document.querySelector('.padded-cont');
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    function handleMediaQueryChange(e) {
+        if (e.matches) {
+            footerContent.insertBefore(themeBtn, footerContent.children[1]);
+        } else {
+            paddedCont.appendChild(themeBtn);
+        }
+    }
+
+    handleMediaQueryChange(mediaQuery);
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+}
+
+document.addEventListener('DOMContentLoaded', moveThemeButton);
