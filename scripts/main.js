28f695dbef8f3dfccb55bf2bceeb8fca586d8214
cdaley95 <10672938@uvu.edu>
2024-07-18 12:57:10 -0600

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 // Fade out current section, remove active tag, fade in next section, add active tag
+
 function showSection(sectionId) {
     var sections = document.querySelectorAll('.content');
     sections.forEach(function(section) {
@@ -17,9 +18,15 @@ function showSection(sectionId) {
     });
 }
 
+/*
+Changes background video depending on the theme selected, 
+enables and disables theme override while also changing the 
+text in the toggle button.
+*/
+
 function changeTheme() {
     const themeOverride = document.getElementById('themeOverride');
-    const button = document.getElementById('themebtn');
+    const themeBtn = document.getElementById('themebtn');
     const bgVid = document.getElementById('bgVid');
     const bgVideo = document.getElementById('bgVideo');
 
@@ -27,24 +34,26 @@ function changeTheme() {
         bgVid.src = "videos/bgviddark.mp4";
         bgVideo.load();
         themeOverride.disabled = false;
-        button.textContent = 'Light Mode';
+        themeBtn.textContent = 'Light Mode';
     } else {
         bgVid.setAttribute("src", "videos/bgvid.mp4");
         bgVideo.load();
         themeOverride.disabled = true;
-        button.textContent = 'Dark Mode';
+        themeBtn.textContent = 'Dark Mode';
     }
 }
 
+//Moves the theme toggle button to the footer and back upon media query trigger.
+
 function moveThemeButton() {
     const themeBtn = document.getElementById('themebtn');
-    const footerContent = document.querySelector('.footer-content');
+    const footerCont = document.querySelector('.footer-content');
     const paddedCont = document.querySelector('.padded-cont');
     const mediaQuery = window.matchMedia('(max-width: 768px)');
 
     function handleMediaQueryChange(e) {
         if (e.matches) {
-            footerContent.insertBefore(themeBtn, footerContent.children[1]);
+            footerCont.insertBefore(themeBtn, footerCont.children[1]);
         } else {
             paddedCont.appendChild(themeBtn);
         }
